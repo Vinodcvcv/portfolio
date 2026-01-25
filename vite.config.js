a4f0135aec019f1b5eab3e0f54f67ab3bfd938e6
Vinod C V <166534994+Vinodcvcv@users.noreply.github.com>
2026-01-25 07:39:43 +0000

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 // ... other imports
 
 export default defineConfig({
-  plugins: [/* ... your plugins */],
-  server: {
-    host: true // This binds the server to 0.0.0.0
-  }
+  plugins: [react()],
 })
